f0caebc2ff1c19ed4314cd870928026a2a03e4e1
amer200 <amermostaafa@gmail.com>
2022-11-21 18:21:55 +0200

    commit

--- controllers/main.js
+++ controllers/main.js
@@ -1,3 +1,21 @@
-exports.getMainPage = (req, res) => {
-    res.render('main/home')
+const Categ = require('../models/categ');
+const Prod = require('../models/prod');
+exports.getMainPage = async (req, res) => {
+    const categs = await Categ.find();
+    const randProds = await Prod.aggregate([{ $sample: { size: 3 } }]).limit(5);
+    const newProds = await Prod.find().sort({ _id: -1 }).limit(5);
+    res.render('main/home', {
+        categs: categs,
+        randProds: randProds,
+        newProds: newProds
+    })
+}
+exports.getCategPage = async (req, res) => {
+    const id = req.params.id;
+    const categs = await Categ.find();
+    const categ = await Categ.findById(id).populate('prods');
+    console.log(categ);
+    res.render('main/categ', {
+        categs: categs,
+    });
 }
--- routes/main.js
+++ routes/main.js
@@ -3,5 +3,5 @@ const route = express.Router();
 const mainController = require('../controllers/main');
 
 route.get('/', mainController.getMainPage);
-
+route.get('/categ/:id', mainController.getCategPage);
 module.exports = route;
